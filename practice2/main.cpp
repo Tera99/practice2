8817ae8fc88835fa03390eee505fa4459fa6295d
Tera Schaller <tschaller722@gmail.com>
2018-07-03 07:29:11 -0600

    Practicing generic functions. Udemy.com, Advanced C++ Programming Training Course by Infinite Skills

--- practice2/main.cpp
+++ practice2/main.cpp
@@ -4,11 +4,24 @@
 //
 //  Created by Terezie Schaller on 7/3/18.
 //  Copyright © 2018 Terezie Schaller. All rights reserved.
-//
+//  Advanced C++ Programming Training Course by Infinite Skills
+//  Generic Functions, Section 3, Lecture 17
 
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+//generic max function
+template <typename T>
+
+T max (T &arg1, T &arg2){
+    if (arg1 > arg2) {
+        return arg1;
+    } else {
+        return arg2;
+    }
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -16,11 +29,18 @@ int main(int argc, const char * argv[]) {
     
     cout << "Tera is a rockstar!!\n";
     
-    //make some changes
-    const int size = 10;
-    for (int i = 0; i < size; i++){
-        printf("Change some stuff: %d\n", i);
-    }
+    int a = 12;
+    int b = 15;
+    cout << max(a, b) << endl;
+    double x = 123.33;
+    double y = 79.56;
+    cout << max(x, y) << endl;
+    string word1 = "hiking";
+    string word2 = "mountain biking";
+    cout << max( word1, word2) << endl;
+    
+    
+    
     
     return 0;
 }
