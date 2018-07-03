7603bea4ee38c3410ef7b71b210bfc3de563be5e
Tera Schaller <tschaller722@gmail.com>
2018-07-03 06:22:58 -0600

    Initial Commit

---
+++ practice2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice2
+//
+//  Created by Terezie Schaller on 7/3/18.
+//  Copyright © 2018 Terezie Schaller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
